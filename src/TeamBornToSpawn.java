08ea020fd162105ee492b45761ba1cdefaf0ed4a
Mikkel Bentsen <cph-mb781@cphbusiness.dk>
2021-09-16 11:33:05 +0200

    Så er der næsten kronhjort

--- src/TeamBornToSpawn.java
+++ src/TeamBornToSpawn.java
@@ -16,7 +16,28 @@ public class TeamBornToSpawn extends Hacker
     @Override
     public boolean tryToHack(Vault vault)
     {
-        
+        for (int i = 1; i <= 9999; i++) {
+
+            if(vault.isCorrect(i))
+            {
+                System.out.println("Password is: " + i);
+            }
+            else
+            {
+                vault.isCorrect(i);
+            }
+        }
         return false;
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Vault vault = new Vault(20);
+        TeamBornToSpawn tbs = new TeamBornToSpawn(vault);
+        // instantiate teamhackers, start and join
+        System.out.println("Done starting");
+        Thread t1 = new Thread(tbs);
+        t1.start();
+        t1.join();
     }
 }
